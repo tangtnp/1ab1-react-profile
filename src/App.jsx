b94d584e7d6fd011e7d11d21c59bebe57088c679
Cedt tanathorn Piyachart <tangutang.tnp@gmail.com>
2026-01-05 13:36:52 +0700

    chore: initial project from vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>My First React App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
